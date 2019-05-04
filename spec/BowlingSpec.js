f70da75cb9baf12c3a797bb4a4a1cbd63b2430ff
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-04 16:32:38 +0100

    add roll and current score function

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -11,4 +11,17 @@ describe("Bowling", function() {
     });
   });
 
+  describe("roll", function() {
+    it("adds roll to score", function() {
+      bowling.roll(5);
+      expect(bowling.score).toEqual(5);
+    });
+  });
+
+  describe("score", function() {
+    it("gives you the current score", function() {
+      expect(bowling.currentScore()).toEqual(bowling.score);
+    });
+  });
+
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,3 +1,11 @@
 function Bowling() {
   this.score = 0
 };
+
+Bowling.prototype.roll = function (number) {
+  this.score += number;
+};
+
+Bowling.prototype.currentScore = function () {
+  return this.score;
+};
