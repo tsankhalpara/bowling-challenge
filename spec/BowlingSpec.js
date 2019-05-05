c01b6246a37484c2900266ecbf3816049006aca0
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-05 15:05:18 +0100

    remove unit test and add different branch

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -5,29 +5,28 @@ describe("Bowling", function() {
     bowling = new Bowling();
   });
 
-  describe("initial score", function() {
-    it("has a start score of zero", function () {
+  describe("roll", function() {
+    it("throws error when no more rolls available", function() {
+      for (var i = 0; i < 20; i++) bowling.roll(0);
+      expect(bowling.roll(4)).toEqual("no more rolls available");
+    });
+  });
+
+
+  describe("Game over", function() {
+    it("returns true when the game is over", function() {
+      for (var i = 0; i < 20; i++) bowling.roll(0);
+      bowling.endgame();
+      expect(bowling.gameover).toEqual(true);
+    })
+  })
+
+  describe("gutter game", function() {
+    it("rolls 0, 20 times", function() {
+      for (var i = 1; i < 21; i++) { bowling.roll(0);}
       expect(bowling.score).toEqual(0);
     });
   });
 
-  describe("roll", function() {
-    it("adds roll to score", function() {
-      bowling.roll(5);
-      expect(bowling.score).toEqual(5);
-    });
-  });
 
-  describe("score", function() {
-    it("gives you the current score", function() {
-      expect(bowling.currentScore()).toEqual(bowling.score);
-    });
-  });
-
-  describe("gutter game", function() {
-    it("rolls 0, 20 times", function() {
-      for (var i = 1; i < 21; i++) bowling.roll(0);
-      expect(bowling.currentScore()).toEqual(0);
-    });
-  });
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,11 +1,30 @@
 function Bowling() {
-  this.score = 0
+  this.score = 0;
+  this.rolls = [];
+  this.gameover = false;
 };
 
 Bowling.prototype.roll = function (number) {
-  this.score += number;
+  if (this.rolls.length < 20) {
+  this.rolls.push(number);
+} else {
+  return "no more rolls available"
+}
 };
 
-Bowling.prototype.currentScore = function () {
-  return this.score;
+Bowling.prototype.score = function () {
+  var result = 0
+  for (var i = 0; i < 20; i++) {
+    result += this.rolls[i];
+  }
+  this.score += result;
+  return result;
+};
+
+Bowling.prototype.endgame = function () {
+  if (this.rolls.length < 20) {
+    this.gameover = false;
+  } else {
+    this.gameover = true;
+  }
 };
